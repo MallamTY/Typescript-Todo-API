df56a03589e94aed9bc36d1fd2236351dba170eb
MallamTY <sosanyatemitayonurudeen@gmail.com>
2023-01-22 09:13:07 +0100

    First commit -m Todo-API using Typescript

---
+++ src/controller/todoController.ts
@@ -0,0 +1,5 @@
+import { RequestHandler} from "express";
+
+const createTodo: RequestHandler = (req, res, next) => {
+
+}
---
+++ src/routes/todoRoutes.ts
@@ -0,0 +1,14 @@
+import express from 'express';
+
+const router = express.Router();
+
+router.post('/');
+
+router.get('/');
+
+router.patch('/');
+
+router.delete('/');
+
+
+export default router;
